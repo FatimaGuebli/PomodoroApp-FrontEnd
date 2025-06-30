baa025f89f6637003225c1d84a6f234f851fd570
fatimaguebli <bitifati1616@gmail.com>
2025-06-30 12:01:19 +0100

    work on add an existing task to today's tasks list

---
+++ src/Pages/PomodoroPage/TaskSection/SelectExistingTask.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+
+const SelectExistingTask = ({ setSelectedtask }) => {
+  //fetch tasks
+  const [allTasks, setAllTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllTasks = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/tasks");
+
+        if (!response.ok) {
+          throw Error("an error fetching tasks from SelectExistingTask");
+        }
+
+        const allTasksArray = await response.json();
+        setAllTasks(allTasksArray);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchAllTasks();
+  }, []);
+
+  //fetch today's task array
+  const [todaysTasks, setTodaysTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTodaystasks = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/todaystasks");
+
+        if (!response.ok) {
+          throw Error("error fetching todays tasks at SelectExistingsTasks");
+        }
+
+        const todaystasksArray = await response.json();
+        setTodaysTasks(todaystasksArray);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchTodaystasks();
+  }, []);
+
+  //filter non selected tasks into an array
+  const nonSelectedTasks = allTasks.filter(
+    (task) => !todaysTasks.some((t) => String(t.id) == String(task.id))
+  );
+
+  const handleSelectChange = async (e) => {
+    const taskId = e.target.value;
+    setSelectedtask(taskId);
+
+    try {
+      await fetch("http://localhost:3001/todaystasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: String(taskId) }),
+      });
+    } catch (error) {}
+  };
+
+  return <select></select>;
+};
+
+export default SelectExistingTask;
--- src/Pages/PomodoroPage/TaskSection/TaskSection.jsx
+++ src/Pages/PomodoroPage/TaskSection/TaskSection.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DisplayTodaysTasks from "./DisplayTodaysTasks";
 import AddNewTask from "../../../components/AddNewTask";
+import SelectExistingTask from "./SelectexistingTask";
 
 const TaskSection = () => {
   //new task button
@@ -12,13 +13,29 @@ const TaskSection = () => {
 
   //add the new task to be displayed in todaystasksList
   const [newtaskId, setNewtaskId] = useState();
+
+  //select an existing task button
+  const [selectExistingButtonState, setSelectExistingButtonState] =
+    useState(false);
+
+  const handleClickSelectAnExistingTaskButton = () => {
+    const currentState = selectExistingButtonState;
+    setSelectExistingButtonState(!currentState);
+  };
+  //array of the existing selected task
+  const [SelectedTask, setSelectedTask] = useState([]);
   return (
     <section>
       <h2>Today's Tasks</h2>
       <div>
         <button onClick={handleClickNewTaskButton}>new task</button>
         {newTaskButtonState && <AddNewTask setNewtaskId={setNewtaskId} />}
-        <button>add an existing task</button>
+        <button onClick={handleClickSelectAnExistingTaskButton}>
+          add an existing task
+        </button>
+        {selectExistingButtonState && (
+          <SelectExistingTask setSelectedTask={setSelectedTask} />
+        )}
       </div>
       <div>
         <DisplayTodaysTasks newtaskId={newtaskId} />
